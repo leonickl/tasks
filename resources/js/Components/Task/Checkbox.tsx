b8ba6dbb7d8dc4e6df1f9ccd6d18a3f38f52827c
Leo Nickl <leo@leonickl.de>
2025-04-12 21:10:44 +0200

    simplify checkbox

--- resources/js/Components/Task/Checkbox.tsx
+++ resources/js/Components/Task/Checkbox.tsx
@@ -1,36 +1,53 @@
 import Priority from '@/Priority';
 import { Task } from '@/types/Task';
-import { router } from '@inertiajs/react';
 import { useState } from 'react';
 
 export default function Checkbox({ task }: { task: Task }) {
     const [completed, setCompleted] = useState(task.completed);
 
+    function toggle() {
+        setCompleted((old) => !old);
+    }
+
     function onChange(newStatus: boolean) {
         setCompleted(newStatus);
-        router.post(route('task.complete', task.id), { completed: newStatus });
+
+        alert(task.summary);
+
+        // router.post(
+        //     route('task.complete', task.id),
+        //     { completed: newStatus },
+        //     {
+        //         preserveState: true, // Optional: This keeps the current page state while processing the request.
+        //         onSuccess: () => {
+        //             console.log(
+        //                 'POST request was successful, no redirect occurred',
+        //             );
+        //         },
+        //         onError: (errors) => {
+        //             console.error('There was an error:', errors);
+        //         },
+        //     },
+        // );
     }
 
     const priority = new Priority(task.priority);
 
     return (
-        <div className="relative">
-            <input
-                type="checkbox"
-                id="custom-checkbox"
-                checked={completed}
-                onChange={(e) => onChange(e.target.checked)}
-                className="peer hidden"
-            />
-
+        <div>
             <label
                 htmlFor="custom-checkbox"
-                className="border-accent-dark m-[3px] flex h-[15px] w-[15px] cursor-pointer items-center justify-center rounded-[3px] border-2 bg-transparent text-lg font-bold text-transparent peer-checked:leading-[1.4] peer-checked:text-white"
-                style={{ borderColor: priority.color() }}
+                className={`border-accent-dark relative m-[3px] flex h-[15px] w-[15px] cursor-pointer items-center justify-center rounded-[3px] border-2 bg-transparent text-lg text-gray-500`}
+                style={{
+                    borderColor: priority.color(),
+                    backgroundColor: completed
+                        ? priority.color()
+                        : 'transparent',
+                    fontWeight: 'bold',
+                }}
+                onClick={toggle}
             >
-                <span className="pointer-events-none relative -top-[6px] left-[2px] select-none">
-                    &times;
-                </span>
+                {completed ? <>&times;</> : <></>}
             </label>
         </div>
     );
